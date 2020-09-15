6f6d866d8cb667a4468984c19ceb9ff40d68e62b
JackYanzhe <532968876@qq.com>
2020-09-15 18:07:37 +0800

    第二次提交

--- dingding-login-master/src/main/java/Controller/DingDingLoginController.java
+++ dingding-login-master/src/main/java/Controller/DingDingLoginController.java
@@ -238,13 +238,10 @@ public class DingDingLoginController {
 		logger.info("成功进入请求钉钉页1，生成二维码登录页面");
 		String time = System.currentTimeMillis() + "";
         System.out.println(time);
+		//新加修改数据信息
 		StringBuilder stringBuilder = new StringBuilder();
 		String result="";
-		stringBuilder
-				.append("https://oapi.dingtalk.com/connect/qrconnect?appid=dingoawgtaauo7rvbyzaah&")
-				.append("response_type=code&scope=snsapi_login&state=")
-				.append(time)
-				.append("&redirect_uri=" + DingTalkConstant.CALL_BACK_URL);
+		
 		try {
 			result = stringBuilder.toString();
 		} catch (Exception e) {
